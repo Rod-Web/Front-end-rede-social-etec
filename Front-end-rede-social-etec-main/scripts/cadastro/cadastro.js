0fdfb93ec345aae6676e9a141dab6179dee7a195
estrela! 𖤐 <estherbaestero@gmail.com>
2025-10-25 00:57:23 -0300

    atualização do front + tela de cadastro

---
+++ Front-end-rede-social-etec-main/scripts/cadastro/cadastro.js
@@ -0,0 +1,9 @@
+function mostrarParte2(){
+    document.getElementById("parte1").style.display = "none";
+    document.getElementById("parte2").style.display = "block";
+}
+
+function voltarParte1(){
+    document.getElementById("parte2").style.display = "none";
+    document.getElementById("parte1").style.display = "block";
+}
